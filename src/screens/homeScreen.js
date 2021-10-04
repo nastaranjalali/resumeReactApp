a390abe5c4c31a335db7161644a6c45979faa066
nastaranjalali <nastaranjalalibusiness@gmail.com>
2021-10-04 15:25:29 +0330

    fix home screen responsive

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -6,7 +6,7 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TelegramIcon from '@material-ui/icons/Telegram';
 import theme from "../themes/theme";
-import {getTranslate} from "../localization";
+import {getDirection, getTranslate} from "../localization";
 
 
 const useStyles = makeStyles({
@@ -18,34 +18,45 @@ const useStyles = makeStyles({
         alignItems: "center",
         justifyContent: "center"
     },
-    name: {
-        color: "#e52764",
-        [theme.breakpoints.down("xs")]:{
-            display:"block"
-        }
-    },
-    btnContainer :{
-        display:"flex",
-        flexDirection:"row",
+    title:{
+        marginBottom: "15px",
 
     },
-    iconBTN:{
-        borderRadius:"50%",
-        border:"2px solid #37374e",
-        color:"#37374e",
-        margin:"10px",
-        transition:" 200ms ease",
-        '&:hover':{
-            borderColor:"#e52764",
-            transition:" 500ms ease",
-            color:"#e52764",
+    name: {
+        color: "#e52764",
+        [theme.breakpoints.down("sm")]: {
+            display: "block"
+        },
+
+    },
+    desc: {
+        marginBottom: "15px", width: "60%",
+        [theme.breakpoints.down('xs')]: {
+            width: "85%",
+        },
+    },
+    btnContainer: {
+        display: "flex",
+        flexDirection: "row",
+
+    },
+    iconBTN: {
+        borderRadius: "50%",
+        border: "2px solid #37374e",
+        color: "#37374e",
+        margin: "10px",
+        transition: " 200ms ease",
+        '&:hover': {
+            borderColor: "#e52764",
+            transition: " 500ms ease",
+            color: "#e52764",
 
         },
 
     },
-    Icon:{
-        fontSize:"2rem",
-        color:"inherit",
+    Icon: {
+        fontSize: "2rem",
+        color: "inherit",
 
     }
 });
@@ -57,8 +68,9 @@ function Home() {
         <div className={classes.root}>
             <div className={"bg"}>
             </div>
-            <Typography variant={"h1"} style={{marginBottom:"15px"}}>{translate.hi1}<span className={classes.name}>{translate.name}</span>{translate.hi2}</Typography>
-            <Typography variant={"body1"} style={{marginBottom:"15px",width:"60%"}}>{translate.homeDesc}</Typography>
+            <Typography variant={"h1"} className={classes.title}>{translate.hi1}<span
+                className={classes.name}>{translate.name}</span>{translate.hi2}</Typography>
+            <Typography variant={"body1"} className={classes.desc}>{translate.homeDesc}</Typography>
             <div className={classes.btnContainer}>
                 <IconButton className={classes.iconBTN}>
                     <LinkedInIcon className={classes.Icon}/>
